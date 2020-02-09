9e60c50a060b6af91ad1d8885918aa20f90ac7b7
Louie O Hagan <louieohagan101@gmail.com>
2020-02-09 23:37:27 +0000

    Additional styling to Hero Image text and added Javascript function to have smooth scrolling on Learn More button

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+// Adds smoth scrolling transition for Home Page "Learn More" Button
+// Code provided by & Full Credits to W3Schools. Tutorial found at https://www.w3schools.com/howto/howto_css_smooth_scroll.asp
+// scroll-behavior: CSS element not used as it does not support Safari browser which a large amount of mobile users would utilize.
+
+$(document).ready(function(){
+        $("a").on('click', function(event) {
+            if (this.hash !== "new-value") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+            } 
+        });
+    });
